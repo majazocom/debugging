a8f47c533d9455e4892842d98181231a5279b7bd
Maja Gedda <maja.gedda@zocom.se>
2020-10-19 22:37:22 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('click', function(e) {
+    //The code that will be executed on click
+
+    //save button element as a variable
+    let img = document.getElementById('image1');
+
+    //check if the click is on the first image
+    if (e.target == img) {
+        // if it is on the first image - execute this
+        console.log('truuuuue');
+    } else {
+        //if not
+        console.log('faaaalse');
+    }
+});
+
+//set element with id image2 as variable
+let img2 = document.querySelector('image2');
+
+//event listener on image 2
+document.querySelector('#image2').addEventListener('mouseover', function() {
+    img2.style.border = "10px solid hotpink";
+});
+
+//set element with id image3 as variable
+let math = document.querySelector('#image3');
+
+//event listener on image 3
+math.addEventListener('click', function() {
+    let math = 1 + 1;
+});
+
+//log the sum of 1 + 1
+console.log('the sum: ', math);
+
+//event listener on image 4 where the image gets bigger when you click on it
+document.querySelector('#image4').addEventListener('click', enlargen);
+
+const enlargen = function() {
+    document.querySelector('#image4').style.transform = "scale(1.2)";
+};
