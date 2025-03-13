2fa80cf996c94dd8eba957facedd7ae3d32f3552
majazocom <70019958+majazocom@users.noreply.github.com>
2025-03-13 15:23:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,22 @@ document.querySelector('#image4').addEventListener('click', enlargen);
 const enlargen = function() {
     document.querySelector('#image4').style.transform = "scale(1.2)";
 };
+
+// event listener on image 5. Should show a text that you clicked it
+let imageFive = document.getElementById('#image5');
+
+imageFive.addEventListener('click', function() {
+    let text = imageFive.querySelector('p');
+    text.innerHtml = 'You clicked me!';
+});
+
+// event listener on image 6. Should change image source.
+let imgSix = document.querySelector('#image6');
+
+imgSix.addEventListner('click', changePic);
+
+function changePic() {
+    imgSix.src = 'https://example.com/newimage.jpg';
+}
+
+let imgSix = document.querySelector('#image6');
